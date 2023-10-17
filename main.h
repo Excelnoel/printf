c246db36c89ce705f0b6819749d3160b294b4fab
BeautifulAngel246 <favouronuche@gmail.com>
2023-10-17 16:02:52 -0700

    changing things

--- main.h
+++ main.h
@@ -19,11 +19,6 @@ int unsignedint(unsigned int n, char *buff, int *tb, int *b, char t);
 int handlestr(char *s, char *buffer, int *tb, int *b);
 int handleint(int n, char *buffer, int *tb, int *b);
 int noncustomspecifier(va_list args, char c, char *buffer, int *tb, int *b);
-int getflags(const char *format, int *i);
-int getprecision(const char *format, int *i, va_list list);
-int getsize(const char *format, int *i);
-int getwidth(const char *format, int *i, va_list list);
-
 
 /**int hexadecimal(va_list h)
   * struct code_format - Struct format
